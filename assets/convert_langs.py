65ec5dacdc8ec2218eb73ab3dde977a8b6bf971a
Ren-B-7 <127970608+Ren-B-7@users.noreply.github.com>
2026-05-12 14:16:31 +0200

    Reformat language header

--- assets/convert_langs.py
+++ assets/convert_langs.py
@@ -4,16 +4,25 @@ import sys
 
 def convert(input_file, output_file):
     with open(input_file, "r") as f:
-        langs = json.load(f)
+        data = json.load(f)
+        langs = []
+        for i in data:
+            if isinstance(i, list):
+                langs.extend(i)
+            else:
+                langs.append(i)
 
     # Write to C header
     with open(output_file, "w") as f:
         json_str = json.dumps(langs).encode("utf-8")
         f.write("const unsigned char languages_json[] = {")
         for i, b in enumerate(json_str):
-            if i % 12 == 0:
-                f.write("\n\t")
-            f.write(f"0x{b:02x}, ")
+            if i == 0:
+                f.write(f"0x{b:02x}, ")
+            elif i % 6 == 0:
+                f.write("\n    0x{b:02x}, ".format(b=b))
+            else:
+                f.write(f"0x{b:02x}, ")
         f.write("0x00\n};\n")
         f.write("const size_t languages_json_len = " + str(len(json_str)) + ";")
 
